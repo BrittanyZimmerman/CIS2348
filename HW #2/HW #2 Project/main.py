4006de3bd33cb5a6dd76e8bbdc94c9369826eb08
BrittanyZimmerman <136195164+BrittanyZimmerman@users.noreply.github.com>
2023-07-02 19:18:18 -0500

    Create main.py
    Brittany Zimmerman
    2219602
    This is part a. of HW#2.

---
+++ HW #2/HW #2 Project/main.py
@@ -0,0 +1,51 @@
+# Name: Brittany Zimmerman
+# PSID: 2219602
+import datetime
+
+month_def = {
+        "January": "1",
+        "February": "2",
+        "March": "3",
+        "April": "4",
+        "May": "5",
+        "June": "6",
+        "July": "7",
+        "August": "8",
+        "September": "9",
+        "October": "10",
+        "November": "11",
+        "December": "12"
+    }
+
+date = ""
+if __name__ == "__main__":
+    while date != "-1":
+        date = str(input("Enter date in the following format: Month Day, Year."))
+        date_list = date.split(" ")
+        comma_true = 0
+
+        if len(date_list) >= 3:
+            month = date_list[0]
+            day = date_list[1]
+            year = date_list[2]
+            year_len = len(year)
+
+            
+
+            if month in month_def:
+                month_num = month_def[month]
+
+                if day.find(",") >= 0:
+                    comma_true = 1
+                    day = day.replace(',', '')
+                    day_num = int(day)
+                    if year.isdigit():
+                        year_num = int(year)
+                        date_string = year + "-" + month_num + "-" + day
+                        date_date = datetime.datetime.strptime(date_string, '%Y-%m-%d').date()
+
+                        if date_date <= datetime.date.today() and year_len == 4:
+                            print(f'{month_num}/{day_num}/{year_num}.')
+
+                else:
+                    continue
