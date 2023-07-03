26e4a2dbe0116bb42964f1012f36853f69449e7e
BrittanyZimmerman <136195164+BrittanyZimmerman@users.noreply.github.com>
2023-07-02 21:10:32 -0500

    HW#2 Part b.
    Brittany Zimmerman
    PSID: 2219602
    
    Part b retrieve dates from txt.

--- HW #2/HW #2 Project/main.py
+++ HW #2/HW #2 Project/main.py
@@ -17,39 +17,48 @@ month_def = {
         "November": "11",
         "December": "12"
     }
+f = open('inputDates.txt')
+values = f.readline()
+
 
 date = ""
 if __name__ == "__main__":
     new_date_list = ['']
-    while date != "-1":
-        date = str(input("Enter date in the following format: Month Day, Year."))
-        date_list = date.split(" ")
-        comma_true = 0
+    f = open('inputDates.txt').readlines()
 
-        if len(date_list) >= 3:
-            month = date_list[0]
-            day = date_list[1]
-            year = date_list[2]
-            year_len = len(year)
+    for date in f:
+        if date == '-1\n':
+            break
+        else:
 
-            if month in month_def:
-                month_num = month_def[month]
+            date = date.strip('\n')
+            date_list = date.split(" ")
+            comma_true = 0
 
-                if day.find(",") >= 0:
-                    comma_true = 1
-                    day = day.replace(',', '')
-                    day_num = int(day)
-                    if year.isdigit():
-                        year_num = int(year)
-                        date_string = year + "-" + month_num + "-" + day
-                        date_date = datetime.datetime.strptime(date_string, '%Y-%m-%d').date()
+            if len(date_list) >= 3:
+                month = date_list[0]
+                day = date_list[1]
+                year = date_list[2]
+                year_len = len(year)
 
-                        if date_date <= datetime.date.today() and year_len == 4:
-                            full_date = str(month_num) + '/' + day + '/' + year
-                            new_date_list.append(full_date)
+                if month in month_def:
+                    month_num = month_def[month]
 
-                else:
-                    continue
+                    if day.find(",") >= 0:
+                        comma_true = 1
+                        day = day.replace(',', '')
+                        day_num = int(day)
+                        if year.isdigit():
+                            year_num = int(year)
+                            date_string = year + "-" + month_num + "-" + day
+                            date_date = datetime.datetime.strptime(date_string, '%Y-%m-%d').date()
+
+                            if date_date <= datetime.date.today() and year_len == 4:
+                                full_date = str(month_num) + '/' + day + '/' + year
+                                new_date_list.append(full_date)
+
+                    else:
+                        continue
 
     new_date_list.pop(0)
     for date in new_date_list:
